cf27c997b8932ad0996d12b59aff987381ff8e17
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-03-31 00:49:54 -0300

    feat: adicionado o id e as importações do Mongo a interface 'Language'

--- src/main/java/br/com/alura/api/languages/Language.java
+++ src/main/java/br/com/alura/api/languages/Language.java
@@ -1,13 +1,29 @@
 package br.com.alura.api.languages;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "mainLanguages")
 public class Language {
+    
+    @Id
+    private String id;
     private String title;
     private String image;
     private int ranking;
     
-    public Language(String title, String image) {
+    public Language() {
+
+    }
+
+    public Language(String title, String image, int ranking) {
         this.title = title;
         this.image = image;
+        this.ranking = ranking;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getTitle() {
