923c996480342548f23bc3d7a9f4c71e2ffec545
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-03-31 00:51:28 -0300

    feat: criado o controller

--- src/main/java/br/com/alura/api/languages/LanguagesController.java
+++ src/main/java/br/com/alura/api/languages/LanguagesController.java
@@ -1,13 +1,20 @@
 package br.com.alura.api.languages;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguagesController {
 
-    @GetMapping(value="/language-java")
-    public String getMethodName() {
-        return "Oi, Java!";
+    @Autowired
+    private LanguageRepository repository;
+
+    @GetMapping("/languages") // <--- o "value" pode ser omitido
+    public List<Language> languages() {
+        return repository.findAll();
     }
+
 }
